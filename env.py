4539fc6808d5541f370314b284f106dc2f1920b8
memphismalik <thisismemphismalik@gmail.com>
2023-09-15 14:29:30 +0000

    dealing with vercel environment variable

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+AIRTABLE_BASE_ID = os.environ.get('AIRTABLE_BASE_ID')
+AIRTABLE_TABLE_NAME = os.environ.get('AIRTABLE_TABLE_NAME')
+AIRTABLE_TOKEN = os.environ.get('AIRTABLE_TOKEN')
+
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
