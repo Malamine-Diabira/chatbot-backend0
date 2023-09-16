5d0eec12fb7b3e24414ee3a4272a3936075ae6c6
Malamine Diabira <112198746+Malamine-Diabira@users.noreply.github.com>
2023-09-16 12:32:04 +0000

    Update env.py

--- env.py
+++ env.py
@@ -1,7 +1,7 @@
 import os
 
-AIRTABLE_BASE_ID = os.environ.get('AIRTABLE_BASE_ID')
-AIRTABLE_TABLE_NAME = os.environ.get('AIRTABLE_TABLE_NAME')
-AIRTABLE_TOKEN = os.environ.get('AIRTABLE_TOKEN')
+AIRTABLE_BASE_ID = os.environ.get('https://airtable.com/appGlSqGhgP18ZMnJ/tblLEBUlGftyl87lM/viwgVQesfYuBefNeb?blocks=hide')
+AIRTABLE_TABLE_NAME = os.environ.get('Conversations')
+AIRTABLE_TOKEN = os.environ.get('patX6FCk0rIa7Kecw.10830f2c9122e199150a254abb68f18c307f8a6c6b989d35fd354cc106568145')
 
-OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+OPENAI_API_KEY = os.environ.get('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')
